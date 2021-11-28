a926b0a88e4a79ada1b87f083730ac0e210242cc
Dim <diminiminim@gmail.com>
2021-11-27 23:54:46 -0700

    Implemented shapeEdges in meadowGen
    Seems to be slow at large sizes
    
    *I made a mistake while editing and lost the original commit. This should work but may be unexpectedly off in some way.

--- meadowGen.lua
+++ meadowGen.lua
@@ -11,14 +11,9 @@ end
 function Meadow:_create()
   self._map = self:_fillMap(0)
 
-  for x = 1, self._width do
-    for y = 1, self._height do
-      if x == 1 or x == self._width or y == 1 or y == self._height then
-        self._map[x][y] = 1
-      end
-    end
-  end
-
+  self:_bordersToWalls()
+  self:_shapeEdges()
+  self:_blobs()
   return self
 end
 
@@ -33,7 +28,159 @@ function Meadow:_fillMap(value)
   return map
 end
 
-function Meadow:_BordersToWalls()
+
+function Meadow:_bordersToWalls()
+  for x = 1, self._width do
+    for y = 1, self._height do
+      if x == 1 or x == self._width or y == 1 or y == self._height then
+        self._map[x][y] = 1
+      end
+    end
+  end
+end
+
+
+function Meadow:_shapeEdges()
+  local growths = {}
+  growths.motherCells = {}
+  growths.sizes = {}
+
+  local cardinals = {
+    n = {0, -1},
+    e = {1, 0},
+    s = {0, 1},
+    w = {-1, 0},
+  }
+
+  local function vonNeumanNeighborhood(motherCell, size)
+
+    local function recurse(motherCell, size)
+      if size > 0 then
+        for _, cardinal in pairs(cardinals) do
+          local x = motherCell[1] + cardinal[1]
+          local y = motherCell[2] + cardinal[2]
+          if x > 1 and x < self._width and y > 1 and y < self._height then
+            self._map[x][y] = 1
+            recurse({x, y}, size - 1)
+          end
+        end
+      end
+    end
+
+    recurse(motherCell, size)
+  end
+
+
+  local function generateMotherCellOrigins()
+    for x = 2, self._width - 1 do
+      for y = 2, self._height - 1 do
+        if love.math.random() > .6 then
+          if x == 2 or x == self._width - 1 or y == 2 or y == self._height - 1 then
+            table.insert(growths.motherCells, {x,y})
+          end
+        end
+      end
+    end
+  end
+
+  local function determineSizes()
+    for i = 1, #growths.motherCells do
+      local size = 1
+      while size < 6 do
+        if love.math.random() >= .12 then
+          size = size + 1
+        else
+          break
+        end
+      end
+      table.insert(growths.sizes, size)
+    end
+  end
+
+
+  generateMotherCellOrigins()
+  determineSizes()
+
+  for i, v in ipairs(growths.motherCells) do
+    self._map[v[1]][v[2]] = 1
+    vonNeumanNeighborhood(v, growths.sizes[i])
+  end
+
+end
+
+
+
+
+
+
+
+
+function Meadow:_blobs()
+
+  local growths = {}
+  growths.motherCells = {}
+  growths.sizes = {}
+
+  local cardinals = {
+    n = {0, -1},
+    e = {1, 0},
+    s = {0, 1},
+    w = {-1, 0},
+  }
+
+  local function vonNeumanNeighborhood(motherCell, size)
+
+    local function recurse(motherCell, size)
+      if size > 0 then
+        for _, cardinal in pairs(cardinals) do
+          local x = motherCell[1] + cardinal[1]
+          local y = motherCell[2] + cardinal[2]
+          if x > 1 and x < self._width and y > 1 and y < self._height then
+            self._map[x][y] = 1
+            recurse({x, y}, size - 1)
+          end
+        end
+      end
+    end
+
+    recurse(motherCell, size)
+  end
+
+
+  local function generateMotherCellOrigins()
+    for x = 2, self._width - 1 do
+      for y = 2, self._height - 1 do
+        if love.math.random() > .99 then
+          table.insert(growths.motherCells, {x,y})
+        end
+      end
+    end
+  end
+
+  local function determineSizes()
+    for i = 1, #growths.motherCells do
+      local size = 3
+      while size < 1 do
+        if love.math.random() >= .9 then
+          size = size + 1
+        else
+          break
+        end
+      end
+      table.insert(growths.sizes, size)
+    end
+  end
+
+
+  generateMotherCellOrigins()
+  determineSizes()
+
+  for i, v in ipairs(growths.motherCells) do
+    self._map[v[1]][v[2]] = 1
+    vonNeumanNeighborhood(v, growths.sizes[i])
+  end
+
+
 
 end
 
--- meadowLevel.lua
+++ meadowLevel.lua
@@ -1,7 +1,7 @@
 local MeadowGen = require "meadowGen"
 
 local function Meadow(level)
-  local map = MeadowGen(20, 20)
+  local map = MeadowGen(80, 40)
   local map = map:_create()
 
   local function spawnActor(room, actor, x, y)
@@ -12,7 +12,7 @@ local function Meadow(level)
   end
 
   local function populateStartRoom(room)
-    spawnActor(room, game.Player, 5, 5)
+    spawnActor(room, game.Player, 30, 20)
   end
 
   populateStartRoom(startRoom)
