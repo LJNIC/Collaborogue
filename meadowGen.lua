6e15b9f99f5f364bd2fa6b0ffa631c085371b61f
Dim <diminiminim@gmail.com>
2021-11-28 17:46:07 -0700

    Added a room and mob
    The specific implementation is not to my liking but it works and isn't too tied up with other systems.
    
    It is unclear where level gen ends and level population begins.
    
    Ultimately, I feel like some utility classes are in order for the level and gen.

--- meadowGen.lua
+++ meadowGen.lua
@@ -56,10 +56,12 @@ end
 
 function Meadow:_create()
   self._map = self:_fillMap(0)
+  self._rooms = {}
 
   self:_bordersToWalls()
   self:_shapeEdges()
   self:_blobs()
+  self:_randomRoom()
   return self
 end
 
@@ -126,24 +128,10 @@ function Meadow:_shapeEdges()
 end
 
 
-
-
-
-
-
-
 function Meadow:_blobs()
   local growths = {}
   growths.motherCells = {}
   growths.sizes = {}
-
-  local cardinals = {
-    n = {0, -1},
-    e = {1, 0},
-    s = {0, 1},
-    w = {-1, 0},
-  }
-
 
   local function generateMotherCellOrigins()
     for x = 2, self._width - 1 do
@@ -177,6 +165,41 @@ function Meadow:_blobs()
     self._map[v[1]][v[2]] = 1
     self:_vonNeuman(v, growths.sizes[i])
   end
+end
+
+function Meadow:_room(xPrime, yPrime, width, height)
+
+  for x = xPrime, xPrime + width do
+    for y = yPrime, yPrime + width do
+      if x == xPrime or x == xPrime + width or y == yPrime or y == yPrime + height then
+        self._map[x][y] = 1
+      else
+        self._map[x][y] = 0
+      end
+    end
+  end
+
+
+  --Cross Pattern
+  for x = xPrime, xPrime + width do
+    for y = yPrime, yPrime + height do
+      if x == xPrime + (width/2) or y == yPrime + (height/2) then
+        self._map[x][y] = 0
+      end
+    end
+  end
+end
+
+function Meadow:_randomRoom()
+
+  local x = love.math.random(2, self._width - 8)
+  local y = love.math.random(2, self._height - 8)
+  local height = 8
+  local width = 8
+
+  table.insert(self._rooms, {x = x, y = y, width = width, height = height})
+
+  self:_room(x, y, width, height)
 end
 
 return Meadow
--- meadowLevel.lua
+++ meadowLevel.lua
@@ -3,20 +3,52 @@ local MeadowGen = require "meadowGen"
 local function Meadow(level)
   local map = MeadowGen(80, 40)
   local map = map:_create()
+  local rooms = map._rooms
 
-  local function spawnActor(room, actor, x, y)
+  local function getRandomEmptySpace()
+
+    local x = nil
+    local y = nil
+
+    repeat
+      x = love.math.random(1, map._width - 1)
+      y = love.math.random(1, map._height - 1)
+    until map._map[x][y] == 0
+
+    return x, y
+  end
+
+  local function spawnActor(actor, x, y)
     local x, y = x or _x, y or _y
     actor.position.x = x
     actor.position.y = y
     level:addActor(actor)
   end
 
-  local function populateStartRoom(room)
-    spawnActor(room, game.Player, 30, 20)
+  local function populateStairRoom(room)
+    local function spawnStairs()
+      local x = room.x + (room.width/2)
+      local y = room.y + (room.height/2)
+      spawnActor(actors.Stairs(), x, y)
+    end
+
+    local function spawnDoors()
+      local centerX = room.x + (room.width/2)
+      local centerY = room.y + (room.height/2)
+
+      spawnActor(actors.Door(), room.x, centerY)
+      spawnActor(actors.Door(), centerX, room.y)
+      spawnActor(actors.Door(), room.x + room.width, centerY)
+      spawnActor(actors.Door(), centerX, room.y + room.height)
+    end
+
+    spawnStairs()
+    spawnDoors()
   end
 
-  populateStartRoom(startRoom)
-
+  spawnActor(game.Player, getRandomEmptySpace())
+  spawnActor(actors.Webweaver(), getRandomEmptySpace())
+  populateStairRoom(rooms[1])
   return map
 end
 
